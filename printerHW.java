1c4355089273ebc84513e4e5dd7fb83c1a3a3a70
Salamy10 <samuel.buerkle@gmail.com>
2026-02-03 10:24:43 +0100

    Create printerHW.java

---
+++ printerHW.java
@@ -0,0 +1,11 @@
+public class printHW {
+
+    public static void main(String[] args) {
+        printer.printHelloWorld();
+        printer.printHelloWorld();
+        printer.printHelloWorld();
+        printer.printHelloWorld();
+        printer.printHelloWorld();
+    }   //Quellcode in statische Methode gekapselt -> fehler können in der "Bibliothek" korrigiert werden
+    
+}
