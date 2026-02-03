a9487e7ae5afb28b6910e5806b6f8abc82c834f5
Salamy10 <samuel.buerkle@gmail.com>
2026-02-03 10:23:50 +0100

    Create printer.java

---
+++ statische methoden /printer.java
@@ -0,0 +1,16 @@
+public class printer {
+
+    public static void printHelloWorld(){
+            
+        System.out.println("Hello World");
+    
+    }
+
+    public static void printtext(String text){
+
+        System.out.println(text);
+    }
+
+    //kann als "bibliotheks"-klasse verwendet werden. auf diese können andere klassen zugreifen
+    
+}
