0d99f608147db051f6017e5b2140544ea5b975d0
Salamy10 <samuel.buerkle@gmail.com>
2026-02-03 12:14:37 +0100

    Create PD.java

---
+++ Vorlesung2/PrimitiveDatatypes/PD.java
@@ -0,0 +1,14 @@
+public class D03_PrimitiveDataTypes{
+
+  public static void main(String[] args){
+
+    //ganze zahlen
+    byte byte1 = 67;
+    short short1 = 22_743;
+    int int1 = 1_000_000;
+    long long1 = 5_000_000_000L;    //"L" an die zahl hängen um "long" zu deklarieren
+
+    
+  }
+  
+}
