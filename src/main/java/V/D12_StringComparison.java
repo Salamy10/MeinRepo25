5b8512f683dbcb008baac0d2dffe54776008bbad
Salamy10 <samuel.buerkle@gmail.com>
2026-02-10 10:09:50 +0100

    Create D12_StringComparison.java

---
+++ src/main/java/V/D12_StringComparison.java
@@ -0,0 +1,32 @@
+package P1;
+
+import java.util.Scanner;
+
+public class D12_StringComparison {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Bitte die Zeichenkette \"true\" eingeben: ");
+		String input = scanner.next();
+
+		if (input == "true") {		//primitive datentypen mit "=="
+
+			System.out.println("Gut gemacht");
+
+		} else {
+			System.out.println("Leider falsch");
+		}
+		
+		
+		if(input.equals("true")) {	//String = Klasse => "v.equals("bedingung")"
+
+			System.out.println("Gut gemacht");
+
+		} else {
+			System.out.println("Leider falsch");
+		}
+
+	}
+}
