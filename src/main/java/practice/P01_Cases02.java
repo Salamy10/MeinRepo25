cd8e8cfd1fb08743c868f67334ffdc5bb09d06a6
Salamy10 <samuel.buerkle@gmail.com>
2026-02-12 12:35:11 +0100

    Refactor operator handling to use char comparison

--- src/main/java/practice/P01_Cases02.java
+++ src/main/java/practice/P01_Cases02.java
@@ -6,26 +6,26 @@ public class P01_Cases02 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("Bitte gib eine ganze Zahl ein: ");
 		int a = scanner.nextInt();
 		System.out.print("Bitte gib einen Operator ein: ");
-		String operator = scanner.next();
+		char operator = scanner.next().charAt(0);
 		System.out.print("Bitte gib eine weitere ganze Zahl ein: ");
 		int b = scanner.nextInt();
-		
-		if (operator.equals("+")) {
-			System.out.println(a + "+" + b + "=" + (a+b));}
-		else if (operator.equals("-")) {
-			System.out.println(a + "-" + b + "=" + (a-b));}
-		else if (operator.equals("*")) {
-			System.out.println(a + "*" + b + "=" + a*b);}
-		else if (operator.equals("/")) {
-			System.out.println(a + "/" + b + "=" +  (double) a/b);}
-		else { System.out.println("unzulässige eingabe");
-		
+
+		if (operator == '+') {
+			System.out.println(a + "+" + b + "=" + (a + b));
+		} else if (operator == '-') {
+			System.out.println(a + "-" + b + "=" + (a - b));
+		} else if (operator == '*') {
+			System.out.println(a + "*" + b + "=" + a * b);
+		} else if (operator == '/') {
+			System.out.println(a + "/" + b + "=" + (double) a / b);
+		} else {
+			System.out.println("unzulässige eingabe");
+
+		}
 
 	}
-
-}
 }
