a75ce10eca98f89b6a047e7ef5216fe6eb51dc57
Salamy10 <samuel.buerkle@gmail.com>
2026-02-19 09:12:14 +0100

    Add Flight class with flight details

---
+++ src/main/java/model/Flight.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Flight {
+	
+	public FlightConnection flightConnection;	//Typisierung einer Eigenschaft
+	public LocalDate flightDate;
+	public double flightPriceInEuro;
+	
+	
+	
+
+}
