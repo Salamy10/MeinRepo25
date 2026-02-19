dcac61021c31c379c85bf0ae98b805e88d813d03
Salamy10 <samuel.buerkle@gmail.com>
2026-02-19 13:06:21 +0100

    Create LightBulb.java

---
+++ src/main/java/model/LightBulb.java
@@ -0,0 +1,15 @@
+package model;
+
+public class LightBulb {
+
+	private String color;
+
+	public void setColor(String color) {
+		this.color = color;	//selbst-referenz
+	}
+	
+	public String getColor() {
+		return color;
+	}
+
+}
