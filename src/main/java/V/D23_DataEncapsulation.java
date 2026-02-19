775061611a952c7555f5409df3b6c1ee4d25b7d9
Salamy10 <samuel.buerkle@gmail.com>
2026-02-19 13:05:43 +0100

    Create D23_DataEncapsulation.java

---
+++ src/main/java/V/D23_DataEncapsulation.java
@@ -0,0 +1,20 @@
+package V;
+
+import model.LightBulb;
+import model.TableLight;
+
+public class D23_DataEncapsulation {
+	public static void main(String[] args) {
+		LightBulb redLightBulb = new LightBulb();
+		redLightBulb.setColor("rot");		//zu selbst-referenz von "LightBulb.java" zugreifen und setzen
+		
+		TableLight light1 = new TableLight();
+		light1.switchOn();	//Methoden setzen anstatt attribute direkt
+		light1.plugIn();
+		light1.changeLightBulb(redLightBulb);
+		
+		System.out.println(light1.isShining());
+		System.out.println(light1.getLightBulb().getColor());
+	}
+
+}
