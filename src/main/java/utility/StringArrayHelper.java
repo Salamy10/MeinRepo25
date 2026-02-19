d67132109a8d351c083c9b0589633088f0baeb90
Salamy10 <samuel.buerkle@gmail.com>
2026-02-19 12:29:14 +0100

    StringArrayHelper.java upload

---
+++ src/main/java/utility/StringArrayHelper.java
@@ -0,0 +1,49 @@
+package Utility;
+
+public class StringArrayHelper {
+
+	public static String[] add(String[] array, String value) {
+
+		int size = StringArrayHelper.size(array);
+		if (size == array.length) { // letzte schublade nicht leer = letzte schublade befüllt = array voll
+			String[] tmp = new String[array.length * 2]; // verdopple arraygröße
+			for (int i = 0; i < array.length; i++) {
+				tmp[i] = array[i];
+			}
+			array = tmp;
+		}
+
+		for (int i = 0; i < array.length; i++) {
+			String content = array[i];
+			if (content == null) {
+				array[i] = value; // wenn leer -> packe wert rein
+				break;
+			}
+		}
+		return array;
+
+	}
+
+	public static int size(String[] array) {
+		int size = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			String content = array[i];
+			if (content != null) {
+				size++;
+			}
+		}
+		return size;
+	}
+	
+	
+	public static void print(String[] array) {
+		System.out.print("[");
+		for (int i = 0; i < array.length; i++) {
+			String content = array[i];
+			System.out.print(content + ", ");
+		}
+		System.out.println("]");
+	}
+
+}
