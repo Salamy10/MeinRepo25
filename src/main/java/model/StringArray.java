ed2ec7ba01386cc6d0c93b0aea86387d6c84b663
Salamy10 <samuel.buerkle@gmail.com>
2026-02-19 13:06:37 +0100

    Create StringArray.java

---
+++ src/main/java/model/StringArray.java
@@ -0,0 +1,52 @@
+package model;
+
+import Utility.StringArrayHelper;
+
+public class StringArray {
+
+	String[] array = new String[2]; // Array vordeklariert
+
+	public void add(String value) {
+
+		int size = size();
+		if (size == array.length) { // letzte schublade nicht leer = letzte schublade befüllt = array voll
+			String[] tmp = new String[array.length * 2]; // verdopple arraygröße
+			for (int i = 0; i < array.length; i++) {
+				tmp[i] = array[i];
+			}
+			array = tmp;
+		}
+
+		for (int i = 0; i < array.length; i++) {
+			String content = array[i];
+			if (content == null) {
+				array[i] = value; // wenn leer -> packe wert rein
+				break;
+			}
+		}
+
+	}
+
+	public int size() {
+		int size = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			String content = array[i];
+			if (content != null) {
+				size++;
+			}
+		}
+		return size;
+	}
+	
+	
+	public void print() {
+		System.out.print("[");
+		for (int i = 0; i < array.length; i++) {
+			String content = array[i];
+			System.out.print(content + ", ");
+		}
+		System.out.println("]");
+	}
+
+}
