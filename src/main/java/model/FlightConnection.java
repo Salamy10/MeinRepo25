ea84f76ae9d0532969ede6b07350cb8fde19273f
Salamy10 <samuel.buerkle@gmail.com>
2026-02-19 09:12:33 +0100

    Add FlightConnection class with flight details

---
+++ src/main/java/model/FlightConnection.java
@@ -0,0 +1,16 @@
+package model;
+
+public class FlightConnection {
+
+//	public static void main(String[] args) {		//ohne das, da klassen = Ziel
+//		// TODO Auto-generated method stub
+//		}
+
+	
+	public String airline;
+	public String airport;
+	public String connectedID;
+	public String arrivalAirport;
+	public String departureAirport;
+	
+}
