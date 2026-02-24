18654731042be864a6ce00adb341a8c7628b5b58
Salamy10 <samuel.buerkle@gmail.com>
2026-02-24 11:25:46 +0100

    Update D20_Classes.java

--- src/main/java/V/D20_Classes.java
+++ src/main/java/V/D20_Classes.java
@@ -1,36 +1,38 @@
 package V;
 
-import model.LightBulb;
 import model.TableLight;
 
+/**
+ * Classes
+ *
+ * @author Daniel Appenmaier
+ * @version 3.0
+ *
+ */
 public class D20_Classes {
 
-	public static void main(String[] args) {
+   public static void main(String[] args) {
+      /* version 2: LightBulb redLightBulb = new LightBulb(); */
+      /* version 1: redLightBulb.color = "rot"; */
 
-		LightBulb redLightBulb = new LightBulb();
-//		redLightBulb.color = "rot";	//version 1
+      /* version 2: LightBulb blueLightBulb = new LightBulb(); */
+      /* version 1: blueLightBulb.color = "blau"; */
 
-		LightBulb blueLightBulb = new LightBulb();
-//		blueLightBulb.color = "blau";	//version 1
+      TableLight tableLight = new TableLight();
+      System.out.println(tableLight.isShining());
+      tableLight.plugIn();
+      System.out.println(tableLight.isShining());
+      tableLight.switchOn();
+      System.out.println(tableLight.isShining());
+      /* version 2: tableLight.changeLightBulb(blueLightBulb); */
+      System.out.println(tableLight.isShining());
+      /* version 1: System.out.println(tableLight.lightBulb.color); */
+      /* version 2: LightBulb oldLightBulb = tableLight.changeLightBulb(redLightBulb); */
+      System.out.println(tableLight.isShining());
+      /* version 1: System.out.println(tableLight.lightBulb.color); */
+      /* version 1: System.out.println(oldLightBulb.color); */
 
-		TableLight tableLight = new TableLight();
-		System.out.println(tableLight.isShining());
-		tableLight.plugIn();
-		System.out.println(tableLight.isShining());
-		tableLight.switchOn();
-		System.out.println(tableLight.isShining());
-		tableLight.changeLightBulb(blueLightBulb);
-		System.out.println(tableLight.isShining());
-//		System.out.println(tableLight.lightBulb.color);		//version 1
-
-		System.out.println();
-		LightBulb oldLightBulb = tableLight.changeLightBulb(redLightBulb);
-		System.out.println(tableLight.isShining());
-//		System.out.println(tableLight.lightBulb.color);		//version 1
-//		System.out.println(oldLightBulb.color);		//version 1
-
-		System.out.println(redLightBulb);
-
-	}
+      System.out.println(tableLight);
+   }
 
 }
