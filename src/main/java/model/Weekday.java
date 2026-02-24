2445d37d1f849c8bbfda7ddd5c952ea0985f818c
Salamy10 <samuel.buerkle@gmail.com>
2026-02-24 13:07:33 +0100

    Create Weekday.java

---
+++ src/main/java/model/Weekday.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum Weekday {	//zeigt minimum
+	
+	MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+	//nicht mal semikolon wenn einfach nur reine Aufzählung
+
+}
