ae8d5463821de14b1dafdd5e7cbbaa5e48873460
Salamy10 <samuel.buerkle@gmail.com>
2026-02-24 11:24:53 +0100

    Create D26_toString.java

---
+++ src/main/java/V/D26_toString.java
@@ -0,0 +1,27 @@
+package V;
+
+import model.TableLight;
+
+import java.util.ArrayList;
+
+import model.LightBulb;
+
+public class D26_toString {
+
+	public static void main(String[] args) {
+		ArrayList<TableLight> lights = new ArrayList<>();
+		LightBulb redLightBulb = new LightBulb("rot");
+		TableLight light1 = new TableLight(redLightBulb);
+		TableLight light2 = new TableLight();
+
+		lights.add(light1);
+		lights.add(light2);
+
+		System.out.println(redLightBulb);
+		System.out.println(light1.toString()); // expliziter Aufruf der toString-Methode
+		System.out.println(light2); // impliziter Aufruf der toString-MethodeS
+		System.out.println(lights);
+
+	}
+
+}
